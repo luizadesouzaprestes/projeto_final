393142fd00143b41e9a2fbe6440846735e412300
luizaprestes <luizadesouzaprestes@gmail.com>
2026-08-19 21:16:03 -0300

    criação HTML telas de cadastro, edição e sobre, ajustes CSS para contemplar essas telas

--- script.js
+++ script.js
@@ -3,15 +3,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const listaMenu = document.getElementById('lista_menu');
 
     if (btnMenu && listaMenu) {
-        // Ao clicar no botão, liga/desliga a classe 'ativo'
         btnMenu.addEventListener('click', (event) => {
-            event.stopPropagation(); // Evita que o clique feche o menu imediatamente
+            event.stopPropagation();
             listaMenu.classList.toggle('ativo');
         });
 
-        // Se clicar em qualquer parte da tela, fecha o menu
         document.addEventListener('click', () => {
             listaMenu.classList.remove('ativo');
         });
+    }
+
+    const radioArtista = document.getElementById('radio_artista');
+    const radioVisitante = document.getElementById('radio_visitante');
+    const campoCodigoConvite = document.getElementById('campo_codigo_convite');
+
+    if (radioArtista && radioVisitante && campoCodigoConvite) {
+        function alternarCampoConvite() {
+            if (radioArtista.checked) {
+                campoCodigoConvite.style.display = 'flex';
+            } else {
+                campoCodigoConvite.style.display = 'none';
+            }
+        }
+
+        // Eventos para verificar a mudança do radio button
+        radioArtista.addEventListener('change', alternarCampoConvite);
+        radioVisitante.addEventListener('change', alternarCampoConvite);
+
+        // Executa uma vez no carregamento para garantir o estado inicial
+        alternarCampoConvite();
     }
 });
